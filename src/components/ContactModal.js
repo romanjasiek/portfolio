998d4a7b10f65e53fe93933f5d44dcb611c2f2be
Roman Jasiek <roman.jasiek@avalost.de>
2021-10-11 20:53:03 +0200

    Started working on contact form

--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
 const ContactModal = () => {
+
+    const [state, handleSubmit] = useForm("xrgrdvvj");
+    if (state.succeeded) {
+        return <p>Thanks for the message!</p>;
+    }
+
     return(
         <>
             <section className="contact-modal" id="contact-modal">
         <div className="contact-modal__content">
             <a className="contact-modal__close" href="#">x</a>
             <h3 className="contact-modal__heading">Get In Touch</h3>
-            <form className="form" action="#">
+            <form className="form" onSubmit={handleSubmit}>
                 <div className="form__horizontal">
                 <div className="form__group">
                     <input type="text" className="form__input" id="contact-modal-name" placeholder="Name" autoComplete="new-password" />
@@ -15,10 +22,20 @@ const ContactModal = () => {
                 </div>
                 <div className="form__group">
                     <input type="email" className="form__input" id="contact-modal-email" placeholder="E-Mail" autoComplete="new-password" />
+                    <ValidationError 
+                        prefix="Email" 
+                        field="contact-modal-email"
+                        errors={state.errors}
+                    />
                     <label className="form__label" htmlFor="contact-modal-email">E-Mail</label>
                 </div>
                 <div className="form__group">
                     <textarea className="form__textarea" id="contact-modal-message" placeholder="Message"></textarea>
+                    <ValidationError 
+                        prefix="Message" 
+                        field="contact-modal-message"
+                        errors={state.errors}
+                    />
                     <label className="form__label" htmlFor="contact-modal-message">Message</label>
                 </div>
                 </div>
@@ -35,7 +52,7 @@ const ContactModal = () => {
                 </div>
 
                 <div className="contact-modal__submit-container">
-                    <input type="submit" className="button button--form" value="Send!" />
+                    <input type="submit" className="button button--form" value="Send!" disabled={state.submitting}/>
                 </div>
             </form>
             <p className="contact-modal__notice">Please note: Since this website is hosted on GitHub Pages, the only way you can contact me is via <a className="contact-modal__notice-link" href="https://github.com/romanjasiek" target="_blank">GitHub</a> or via <a className="contact-modal__notice-link" href="#getintouch">e-mail</a>.</p>
